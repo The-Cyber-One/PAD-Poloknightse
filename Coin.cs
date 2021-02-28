5ff8ed9b40953b450c4896937d6a89609fa4b7ce
Saad Zetouny <saad.zetouny@hva.nl>
2021-02-28 19:55:28 +0100

    Score toegevoegd

--- Coin.cs
+++ Coin.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace BaseProject
 {
     class Coin: GameObject
     {
+        public int score;
         public Coin() : base("Coin/bronze_coin")
         {
 
@@ -14,6 +17,15 @@ namespace BaseProject
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            if (GameEnvironment.KeyboardState.IsKeyDown(Keys.Space))
+            {
+                score++;
+                Debug.WriteLine(score);
+            }
+        }
+        public void Reset()
+        {
+            score = 0;
         }
     }
 }
