73ecdb713914fb333d39aac55b70fac06582294e
Martijn Zwart <martijnzwart@hotmail.nl>
2021-03-11 11:07:42 +0100

    HealthPickup class gemaakt

---
+++ GameObjects/HealthPickup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+namespace BaseProject.GameObjects
+{
+    class HealthPickup : GameObject
+    {
+
+        public HealthPickup(Vector2 gridPosition) : base(gridPosition)
+        {
+
+        }
+    }
+}
