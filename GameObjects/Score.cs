4925627f250b4615070bed5380b23e826bd7f93d
Joshua Knaven <joshua.knaven@gmail.com>
2021-03-15 16:10:26 +0100

    ligte clean up

--- GameObjects/Score.cs
+++ GameObjects/Score.cs
@@ -4,20 +4,22 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace BaseProject.GameObjects
+namespace BaseProject
 {
     class Score 
     {
         SpriteFont spriteFont;
         public Vector2 position;
-        public String text;
+        public string text;
         public int score;
+        
         public Score()
         {
             spriteFont = GameEnvironment.ContentManager.Load<SpriteFont>("GameObjects/GameFont");
             position.X = 10;
             position.Y = 10;
         }
+
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.DrawString(spriteFont,text, position, Color.White);
