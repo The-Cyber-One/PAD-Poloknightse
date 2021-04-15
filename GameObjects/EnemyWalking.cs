909afac6f9f9d82e7b492556163698dc5fa670a8
Joshua Knaven <joshua.knaven@gmail.com>
2021-04-15 12:15:54 +0200

    Code opgeschoont door AStar te splitsen en een StateMachine class te makken

--- GameObjects/EnemyWalking.cs
+++ GameObjects/EnemyWalking.cs
@@ -2,109 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Priority_Queue;
 
 namespace Poloknightse
 {
-    class EnemyWalking : GameObject
+    class PatrolState : State
     {
-        int chaseStamina = 5;
-        int stamina;
-        int patrolStamina = 3;
-        Point startPosition;
+        public int stamina;
+        private const int MAX_STAMINA = 5;
+        GameObject gameObject;
 
-        Point[] path;
-
-        State currentState;
-
-        enum State
+        public PatrolState(GameObject gameObject) : base("Patrol")
         {
-            Patrol,
-            Chase,
-            Return,
-            Crying
+            this.gameObject = gameObject;
         }
 
-        public EnemyWalking(Point gridPosition) : base(gridPosition, "GameObjects/Player/Koning")
+        public override void Start()
         {
-            startPosition = gridPosition;
-            SetState(State.Patrol);
+            stamina = MAX_STAMINA;
         }
 
         public override void FixedUpdate(GameTime gameTime)
         {
-            base.FixedUpdate(gameTime);
+            stamina--;
 
-            if (!CanMove())
+            Point direction = GetRandomDirection();
+            while (LevelLoader.grid[gameObject.gridPosition.X + direction.X, gameObject.gridPosition.Y + direction.Y].tileType == Tile.TileType.WALL)
             {
-                SetState(State.Crying);
+                direction = GetRandomDirection();
             }
-
-            switch (currentState)
-            {
-                case State.Patrol:
-                    {
-                        if (stamina <= 0)
-                        {
-                            SetState(State.Chase);
-                            break;
-                        }
-                        stamina--;
-
-                        Point direction = GetRandomDirection();
-                        while (LevelLoader.grid[gridPosition.X + direction.X, gridPosition.Y + direction.Y].tileType == Tile.TileType.WALL)
-                        {
-                            direction = GetRandomDirection();
-                        }
-                        gridPosition += direction;
-                        break;
-                    }
-                case State.Chase:
-                    {
-                        if (path == null)
-                        {
-                            path = AStar(gridPosition, getPlayerPosition(), h);
-                            if (chaseStamina >= path.Length) stamina = path.Length - 1;
-                            else stamina = chaseStamina;
-                        }
-
-                        if (stamina <= 0)
-                        {
-                            SetState(State.Return);
-                            break;
-                        }
-                        stamina--;
-
-                        gridPosition = path[path.Length + stamina - chaseStamina - 1];
-
-                        break;
-                    }
-
-                case State.Return:
-                    {
-                        if (path == null)
-                        {
-                            path = AStar(gridPosition, startPosition, h);
-                            stamina = path.Length - 1;
-                        }
-
-                        if (stamina <= 0)
-                        {
-                            SetState(State.Patrol);
-                            break;
-                        }
-                        stamina--;
-
-                        gridPosition = path[stamina];
-                        break;
-                    }
-            }
-        }
-
-        private Point getPlayerPosition()
-        {
-            //Maybe add for loop to find nearest player segment
-            return (GameEnvironment.CurrentGameState as PlayingState).player.gridPosition;
+            gameObject.gridPosition += direction;
         }
 
         private Point GetRandomDirection()
@@ -118,6 +44,115 @@ namespace Poloknightse
                 return new Point(0, GameEnvironment.Random.Next(2) * 2 - 1);
             }
         }
+    }
+
+    class ChaseState : State
+    {
+        public int stamina;
+        private const int MAX_STAMINA = 5;
+        GameObject gameObject, player;
+        Point[] path;
+
+        public ChaseState(GameObject gameObject, GameObject player) : base("Chase")
+        {
+            this.gameObject = gameObject;
+            this.player = player;
+        }
+
+        public override void Start()
+        {
+            path = AStar.FindPath(gameObject.gridPosition, player.gridPosition);
+
+            if (MAX_STAMINA >= path.Length) stamina = path.Length - 1;
+            else stamina = MAX_STAMINA;
+        }
+
+        public override void FixedUpdate(GameTime gameTime)
+        {
+            stamina--;
+
+            gameObject.gridPosition = path[path.Length + stamina - MAX_STAMINA - 1];
+        }
+    }
+
+    class ReturnState : State
+    {
+        public int stamina;
+        Point startPosition;
+        GameObject gameObject;
+        Point[] path;
+
+        public ReturnState(GameObject gameObject) : base("Return")
+        {
+            startPosition = gameObject.gridPosition;
+            this.gameObject = gameObject;
+        }
+
+        public override void Start()
+        {
+            Point gridPosition = gameObject.gridPosition;
+            path = AStar.FindPath(gridPosition, startPosition);
+            stamina = path.Length - 1;
+        }
+
+        public override void FixedUpdate(GameTime gameTime)
+        {
+            stamina--;
+            gameObject.gridPosition = path[stamina];
+        }
+    }
+
+    class CryingState : State
+    {
+        GameObject gameObject;
+
+        public CryingState(GameObject gameObject) : base("Crying")
+        {
+            this.gameObject = gameObject;
+        }
+
+        public override void Start()
+        {
+            System.Diagnostics.Debug.WriteLine(":(");
+        }
+    }
+
+    class EnemyWalking : GameObject
+    {
+        StateMachine stateMachine;
+
+        public EnemyWalking(Point gridPosition) : base(gridPosition, "GameObjects/Player/Koning")
+        {
+            stateMachine = new StateMachine();
+
+            //Add states to stateMachine
+            stateMachine.AddState(new PatrolState(this));
+            stateMachine.AddState(new ChaseState(this, (GameEnvironment.CurrentGameState as PlayingState).player));
+            stateMachine.AddState(new ReturnState(this));
+            stateMachine.AddState(new CryingState(this));
+            //stateMachine.AddState(new SearchState());
+
+            //Make a function to check stamina in the states
+            Func<object, bool> StaminaCheck = new Func<object, bool>((object stamina) => (int)stamina <= 0);
+
+            //Add connections between states
+            stateMachine.AddConnection("Patrol", "Return", StaminaCheck, ref (stateMachine.GetState("Patrol") as PatrolState).stamina);
+            stateMachine.AddConnection("Chase", "Return", StaminaCheck, ref (stateMachine.GetState("Chase") as ChaseState).stamina);
+            stateMachine.AddConnection("Return", "Patrol", StaminaCheck, ref (stateMachine.GetState("Return") as ReturnState).stamina);
+            stateMachine.AddConnectionToAll("Crying", CanMove);
+
+            //Set state to Patrol
+            stateMachine.SetState("Patrol");
+        }
+
+        public override void FixedUpdate(GameTime gameTime)
+        {
+            base.FixedUpdate(gameTime);
+
+            if (!CanMove()) stateMachine.SetState("Crying");
+
+            stateMachine.FixedUpdate(gameTime);
+        }
 
         private bool CanMove()
         {
@@ -125,108 +160,6 @@ namespace Poloknightse
                 LevelLoader.grid[gridPosition.X - 1, gridPosition.Y].tileType == Tile.TileType.GROUND ||
                 LevelLoader.grid[gridPosition.X, gridPosition.Y + 1].tileType == Tile.TileType.GROUND ||
                 LevelLoader.grid[gridPosition.X, gridPosition.Y - 1].tileType == Tile.TileType.GROUND;
-        }
-
-        private void SetState(State newState)
-        {
-            currentState = newState;
-            path = null;
-            // TODO: Make better state machine
-            stamina = patrolStamina;
-        }
-
-        private List<Point> ReconstructPath(Point[] cameFrom, Point current)
-        {
-            List<Point> totalPath = new List<Point>();
-            totalPath.Add(current);
-
-            //if (cameFrom[CoordsToIndex(current)] != (-Vector2.One).ToPoint())
-            //{
-                while (/*Array.IndexOf(cameFrom, current) != -1 &&*/ cameFrom[CoordsToIndex(current)] != (-Vector2.One).ToPoint())
-                {
-                    current = cameFrom[CoordsToIndex(current)];
-                    totalPath.Add(current);
-                } 
-            //}
-            return totalPath;
-        }
-
-        private float h(Point pointA, Point pointB) => Vector2.Distance(pointA.ToVector2(), pointB.ToVector2());
-
-        private Point[] GetNeighbors(Point center)
-        {
-            List<Point> neighbors = new List<Point>();
-            if (center.X != LevelLoader.grid.GetLength(0) - 1)
-                neighbors.Add(new Point(center.X + 1, center.Y));
-            if (center.Y != LevelLoader.grid.GetLength(1) - 1)
-                neighbors.Add(new Point(center.X, center.Y + 1));
-            if (center.X != 0)
-                neighbors.Add(new Point(center.X - 1, center.Y));
-            if (center.Y != 0)
-                neighbors.Add(new Point(center.X, center.Y - 1));
-
-            return neighbors.ToArray();
-        }
-        private int CoordsToIndex(Point point) => point.X + LevelLoader.grid.GetLength(0) * point.Y;
-
-        // A* finds a path from start to goal.
-        // h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-        private Point[] AStar(Point start, Point goal, Func<Point, Point, float> h)
-        {
-            // The set of discovered nodes that may need to be (re-)expanded.
-            // Initially, only the start node is known.
-            // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-            SimplePriorityQueue<Point> openSet = new SimplePriorityQueue<Point>();
-            openSet.Enqueue(start, 0);
-
-            // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
-            // to n currently known.
-            Point[] cameFrom = new Point[LevelLoader.grid.GetLength(0) * LevelLoader.grid.GetLength(1)];
-
-            // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
-            float[] gScore = new float[LevelLoader.grid.GetLength(0) * LevelLoader.grid.GetLength(1)];
-
-            // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
-            // how short a path from start to finish can be if it goes through n.
-            float[] fScore = new float[LevelLoader.grid.GetLength(0) * LevelLoader.grid.GetLength(1)];
-            for (int i = 0; i < gScore.Length; i++)
-            {
-                cameFrom[i] = (-Vector2.One).ToPoint();
-                gScore[i] = float.PositiveInfinity;
-                fScore[i] = float.PositiveInfinity;
-            }
-            gScore[CoordsToIndex(start)] = 0;
-            fScore[CoordsToIndex(start)] = h(start, goal);
-
-            while (openSet.Count > 0)
-            {
-                // This operation can occur in O(1) time if openSet is a min-heap or a priority queue
-                Point current = openSet.First;
-                if (current == goal)
-                {
-                    return ReconstructPath(cameFrom, current).ToArray();
-                }
-                openSet.Remove(current);
-                foreach (Point neighbor in GetNeighbors(current))
-                {
-                    // d(current,neighbor) is the weight of the edge from current to neighbor
-                    // tentative_gScore is the distance from start to the neighbor through current
-                    float tentative_gScore = gScore[CoordsToIndex(current)];// + 1;
-                    int neighborIndex = CoordsToIndex(neighbor);
-                    if (tentative_gScore < gScore[neighborIndex])
-                    {
-                        // This path to neighbor is better than any previous one. Record it!
-                        cameFrom[neighborIndex] = current;
-                        gScore[neighborIndex] = tentative_gScore;
-                        fScore[neighborIndex] = gScore[neighborIndex] + h(neighbor, goal);
-                        if (!openSet.Contains(neighbor))
-                            openSet.Enqueue(neighbor, 0);
-                    }
-                }
-            }
-            // Open set is empty but goal was never reached
-            SetState(State.Crying);
-            return null;
         }
     }
 }
---
+++ GameObjects/StateMachine.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poloknightse
+{
+    public abstract class State
+    {
+        public Dictionary<string, Tuple<Func<object, bool>, object>> connections;
+
+        public string name;
+
+        public State(string name)
+        {
+            this.name = name;
+            connections = new Dictionary<string, Tuple<Func<object, bool>, object>>();
+        }
+
+        public virtual void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void FixedUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void End()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class StateMachine
+    {
+        private Dictionary<string, State> states = new Dictionary<string, State>();
+        State currentState;
+
+        /// <summary>
+        /// Add a <see cref="State"/> state to the dictiorary with <see cref="State.name"/> as key
+        /// </summary>
+        /// <param name="newState">The state to add</param>
+        public void AddState(State newState)
+        {
+            if (states.ContainsKey(newState.name)) states.Remove(newState.name);
+            states.Add(newState.name, newState);
+        }
+
+        /// <summary>
+        /// Sets the current state
+        /// </summary>
+        /// <param name="newState">The state that will be set</param>
+        public void SetState(string newState)
+        {
+            if (states.ContainsKey(newState))
+            {
+                if (currentState != null) currentState.End();
+                currentState = states[newState];
+                currentState.Start();
+            }
+        }
+
+        /// <summary>
+        /// Gets a state from the dictionary
+        /// </summary>
+        /// <param name="name">The name of the state you wanna get</param>
+        /// <returns>The requested <see cref="State"/></returns>
+        public State GetState(string name)
+        {
+            return states[name];
+        }
+
+        /// <summary>
+        /// Add a connection between states in the dictionary that will switch to the selected state
+        /// </summary>
+        /// <param name="fromState">Connection from this state</param>
+        /// <param name="toState">Connection to this state</param>
+        /// <param name="func">The function that checks when to switch</param>
+        /// <param name="referanceVariables">Variables to keep track of for the function</param>
+        public void AddConnection(string fromState, string toState, Func<object, bool> func, ref int referanceVariables)
+        {
+            if (states.ContainsKey(fromState) && states.ContainsKey(toState))
+            {
+                states[fromState].connections.Add(toState, new Tuple<Func<object, bool>, object>(func, referanceVariables));
+            }
+        }
+
+        /// <summary>
+        /// Add a connection between states in the dictionary that will switch to the selected state
+        /// </summary>
+        /// <param name="fromState">Connection from this state</param>
+        /// <param name="toState">Connection to this state</param>
+        /// <param name="func">The function that checks when to switch</param>
+        public void AddConnection(string fromState, string toState, Func<bool> func)
+        {
+            Func<object, bool> func1 = new Func<object, bool>((object var) => func.Invoke());
+            if (states.ContainsKey(fromState) && states.ContainsKey(toState))
+            {
+                states[fromState].connections.Add(toState, new Tuple<Func<object, bool>, object>(func1, null));
+            }
+        }
+
+        /// <summary>
+        /// Add a connection from all states to <paramref name="toState"/> in the dictionary
+        /// </summary>
+        /// <param name="toState">Connection to this state</param>
+        /// <param name="func">The function that checks when to switch</param>
+        public void AddConnectionToAll(string toState, Func<bool> func)
+        {
+            Func<object, bool> func1 = new Func<object, bool>((object var) => func.Invoke());
+            if (states.ContainsKey(toState))
+            {
+                foreach (string state in states.Keys)
+                {
+                    states[state].connections.Add(toState, new Tuple<Func<object, bool>, object>(func1, null));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update the current state
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            if (currentState != null)
+            {
+                currentState.Update(gameTime);
+                CheckConnections();
+            }
+        }
+
+        /// <summary>
+        /// Update the current state on a fixed time
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void FixedUpdate(GameTime gameTime)
+        {
+            if (currentState != null)
+            {
+                currentState.FixedUpdate(gameTime);
+                CheckConnections();
+            }
+        }
+
+        /// <summary>
+        /// Checks if connection from current state wants to switch
+        /// </summary>
+        private void CheckConnections()
+        {
+            foreach (string otherState in currentState.connections.Keys)
+            {
+                Func<object, bool> func = currentState.connections[otherState].Item1;
+
+                object args = currentState.connections[otherState].Item2;
+                
+                if (func.Invoke(args))
+                {
+                    SetState(otherState);
+                }
+            }
+        }
+    }
+}
