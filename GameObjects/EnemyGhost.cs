fde945062bf883f4fc30822b8d003d2cecbfa9fa
Martijn Zwart <martijnzwart@hotmail.nl>
2021-04-16 14:38:33 +0200

    Clean up - GameObjects heeft folder voor enemy sprites

--- GameObjects/EnemyGhost.cs
+++ GameObjects/EnemyGhost.cs
@@ -11,7 +11,7 @@ namespace Poloknightse
 {
     class EnemyGhost : GameObject
     {
-        public EnemyGhost(Point gridPosition) : base(gridPosition, "GameObjects/EnemyGhost")
+        public EnemyGhost(Point gridPosition) : base(gridPosition, "GameObjects/Enemies/EnemyGhost")
         {
 
         }
