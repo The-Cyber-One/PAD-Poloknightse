65881897270a478713d3ad8ff6cc7cc1aa9c2f02
MeesMD <meesmilan@hotmail.com>
2021-04-21 11:48:56 +0200

    Rework van EnemyShooter
    Did a complete rework of the shooter, now it detects where it is in the level and moves according to that. There is only 1 bug in it now and I know the fix but I need Joshua for it

--- GameObjects/EnemyShooter.cs
+++ GameObjects/EnemyShooter.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Diagnostics;
 
 namespace Poloknightse
 {
-    class EnemyShooter : GameObject
+	class EnemyShooter : GameObject
     {
-        private float countDuration = 5f; //Every  s.
+        private float countDuration = 5f; //Every 5s.
         private float currentTime = 0f;
+        private int xOffsetCheck, yOffsetCheck;
+        private int endCalculationX, endCalculationY;
+        private String enemyPos;
+        private Vector2 shootDir;
 
         public EnemyShooter(Vector2 gridPosition) : base(gridPosition, "GameObjects/Player/Onderbroek_ridder")
         {
-            velocity.Y = 1;
         }
 
         /// <summary>
@@ -25,19 +25,65 @@ namespace Poloknightse
             GameEnvironment.CurrentGameState.gameObjectList.Add(new Bullet(gridPosition, direction));
         }
 
+        private void MovementPathCheck()
+		{
+            int (int)gridPosition.X = (int)gridPosition.X;
+            int (int)gridPosition.Y = (int)gridPosition.Y;
+
+            if (enemyPos == "left" || enemyPos == "right")
+            {
+                xOffsetCheck = 1;
+                yOffsetCheck = 2;
+                endCalculationX = (int)gridPosition.X + xOffsetCheck;
+                endCalculationY = (int)gridPosition.Y - yOffsetCheck;
+            }
+			else if(enemyPos == "top" || enemyPos == "bottom")
+            {
+                xOffsetCheck = 2;
+                yOffsetCheck = 1;
+                endCalculationX = (int)gridPosition.X - xOffsetCheck;
+                endCalculationY = (int)gridPosition.Y + yOffsetCheck;
+            }
+            
+            //check depending on enemyPos what tiles to check for it to detect to move the opposite way.
+            if (LevelLoader.grid[(int)gridPosition.X + xOffsetCheck, (int)gridPosition.Y + yOffsetCheck].tileType != Tile.TileType.WALL ||
+                LevelLoader.grid[endCalculationX, endCalculationY].tileType != Tile.TileType.WALL)
+            {
+                velocity *= -1;
+            }
+			
+        }
+
 		public override void FixedUpdate(GameTime gameTime)
 		{
 			base.Update(gameTime);
 
             gridPosition += velocity;
 
-            //Check where the wall ends and inverse the velocity so it goes the opposite direction.
-            if(LevelLoader.grid[(int)gridPosition.X + 1, (int)gridPosition.Y + 2].tileType != Tile.TileType.WALL||
-               LevelLoader.grid[(int)gridPosition.X + 1, (int)gridPosition.Y - 2].tileType != Tile.TileType.WALL)
-            {
-                velocity *= -1;
+            if (LevelLoader.grid[(int)gridPosition.X + 1, (int)gridPosition.Y].tileType == Tile.TileType.WALL)
+			{
+                enemyPos = "left";
+                shootDir = new Vector2(1, 0);
             }
+            if (LevelLoader.grid[(int)gridPosition.X - 1, (int)gridPosition.Y].tileType == Tile.TileType.WALL)
+            {
+                enemyPos = "right";
+                shootDir = new Vector2(-1, 0);
+            }
+            if (LevelLoader.grid[(int)gridPosition.X, (int)gridPosition.Y + 1].tileType == Tile.TileType.WALL)
+            {
+                enemyPos = "top";
+                shootDir = new Vector2(0, 1);
+            }
+            if (LevelLoader.grid[(int)gridPosition.X, (int)gridPosition.Y - 1].tileType == Tile.TileType.WALL)
+            {
+                enemyPos = "bottom";
+                shootDir = new Vector2(0, -1);
+            }
+
+            MovementPathCheck();
         }
+
 
 		public override void Update(GameTime gameTime)
 		{
@@ -47,7 +93,7 @@ namespace Poloknightse
             if (currentTime >= countDuration)
             {
                 currentTime -= countDuration;
-                Shoot(new Vector2(1, 0));
+                Shoot(shootDir);
             }
         }
     }
