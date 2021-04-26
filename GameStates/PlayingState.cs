34d9cb48dc9bee0e61d1a76bdf4c53765fd52511
Saad Zetouny <saad.zetouny@hva.nl>
2021-04-26 13:00:47 +0200

    level 5 toegevoegd

--- GameStates/PlayingState.cs
+++ GameStates/PlayingState.cs
@@ -15,7 +15,7 @@ namespace Poloknightse
 
         public override void Init()
         {
-            LevelLoader.LoadLevel("Level-2");
+            LevelLoader.LoadLevel("Level-5");
 
             //Count how many coins there are in the level
             for (int i = gameObjectList.Count - 1; i >= 0; i--)
