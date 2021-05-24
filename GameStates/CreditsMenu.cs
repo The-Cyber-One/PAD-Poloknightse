76793f8d5781382893645077efba1fa6bd23d204
Saad Zetouny <saad.zetouny@hva.nl>
2021-05-24 16:15:20 +0200

    CreditsMenu af misschine

--- GameStates/CreditsMenu.cs
+++ GameStates/CreditsMenu.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,6 +15,8 @@ namespace Poloknightse
         public override void Init()
         {
             LevelLoader.LoadLevel("CreditsMenu");
+            gameObjectList.Add(new TextGameObject("Poloknightse\nBy Stinky Koala's", new Vector2(GameEnvironment.Screen.X / 2, GameEnvironment.Screen.Y / 2), Vector2.One / 2, Color.Wheat, "Fonts/Title"));
+            gameObjectList.Add(new TextGameObject("Mees Dekker, Joshua Knaven, Robin de Graaff, Saad Zetouny, Martijn Zwart", new Vector2(GameEnvironment.Screen.X / 2, GameEnvironment.Screen.Y * 2 / 3), Vector2.One / 2, Color.Wheat, "Fonts/Title", 0.7f));
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
