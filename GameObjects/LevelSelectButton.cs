f1aecd9366c818d3857e91179f552edab7ee10eb
TaffelMenn <RobindeGraaff@icloud.com>
2021-06-06 18:33:04 +0200

    Sub classes vol gepropt met comments

--- GameObjects/LevelSelectButton.cs
+++ GameObjects/LevelSelectButton.cs
@@ -11,6 +11,11 @@ namespace Poloknightse
     {
         public int level;
 
+        /// <summary>
+        /// Make a clickable level select button
+        /// </summary>
+        /// <param name="buttonBox">A rectangle for the X-coordinates, Y-coordinates, width and height, respectively</param>
+        /// <param name="level">Which level should the button refer to</param>
         public LevelSelectButton(Rectangle buttonBox, int level) : base(buttonBox, Game1.levels[level] + "Drawn", Game1.levels[level])
         {
             this.level = level;
